8f55c697ad90c11fa14aa98c57aa00dff56e5f6f
MetaJT <jordantrotter3@gmail.com>
2024-02-24 00:36:06 -0600

    Initial Commit

---
+++ LotteryTickets/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LotteryTickets
+//
+//  Created by Jordan Trotter on 2/24/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
